f0c49a2483f6565dfec972a9b95158b73e0adb96
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-08-13 15:01:39 -0700

    feat: ensure FacebookAuthenticationService rerurns AuthenticationError when  LoadFacebookUserApi returns undefined

--- src/application/services/facebook-authentication.ts
+++ src/application/services/facebook-authentication.ts
@@ -1,9 +1,11 @@
 import { FacebookAuthenticationParams } from '@/domain/features'
-import { LoadFacebookUserApi, LoadFacebookUserApiResult } from '@/application/contracts/api'
+import { LoadFacebookUserApi } from '@/application/contracts/api'
+import { AuthenticationError } from '@/domain/errors'
 
 export class FacebookAuthenticationService {
   constructor(private readonly _loadFacebookUserApi: LoadFacebookUserApi) {}
-  async perform(_params: FacebookAuthenticationParams): Promise<LoadFacebookUserApiResult> {
+  async perform(_params: FacebookAuthenticationParams): Promise<AuthenticationError> {
     await this._loadFacebookUserApi.loadUser(_params)
+    return new AuthenticationError()
   }
 }

78d30650eaaed263b30ef3c69419c61f7ab337ca
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-08-13 15:02:47 -0700

    test: ensure FacebookAuthenticationService returns AuthenticationError when  LoadFacebookUserApi returns undefined

--- tests/application/services/facebook-authentication.spec.ts
+++ tests/application/services/facebook-authentication.spec.ts
@@ -1,10 +1,13 @@
 import { LoadFacebookUserApi, LoadFacebookUserApiParams, LoadFacebookUserApiResult } from '@/application/contracts/api'
 import { FacebookAuthenticationService } from '@/application/services'
+import { AuthenticationError } from '@/domain/errors'
 
 class LoadFacebookUserApiSpy implements LoadFacebookUserApi {
   token?: string
+  result: undefined
   async loadUser (params: LoadFacebookUserApiParams ): Promise<LoadFacebookUserApiResult> {
     this.token = params.token
+    return this.result
   }
 }
 
@@ -15,5 +18,12 @@ describe('Facebook Authentication Service', () => {
     await sut.perform({ token: 'any_token' })
     expect(loadFacebookUserApi.token).toBe('any_token')
   })
-})
 
+  it('should return AuthenticationError when LoadFacebookUserApi returns undefined', async () => {
+    const loadFacebookUserApi = new LoadFacebookUserApiSpy()
+    loadFacebookUserApi.result = undefined
+    const sut = new FacebookAuthenticationService(loadFacebookUserApi)
+    const authResult = await sut.perform({ token: 'any_token' })
+    expect(authResult).toEqual(new AuthenticationError())
+  })
+})

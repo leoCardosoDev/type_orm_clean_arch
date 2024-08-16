cfb1d4bcf33dc523bbc465be2f7db84985c6e9f5
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-08-16 15:14:49 -0700

    refactor: remove types for interface doamin

--- src/domain/features/facebook-authentication.ts
+++ src/domain/features/facebook-authentication.ts
@@ -1,5 +1,12 @@
-import { FacebookAuthenticationParams, FacebookAuthenticationResult } from './types'
+import { AuthenticationError } from '@/domain/errors'
+import { AccessToken } from '@/domain/models'
 
 export interface FacebookAuthentication {
   perform: (_params: FacebookAuthenticationParams) => Promise<FacebookAuthenticationResult>
 }
+
+export type FacebookAuthenticationParams = {
+  token: string
+}
+
+export type FacebookAuthenticationResult = AccessToken | AuthenticationError
--- src/domain/features/types.ts
+++
@@ -1,8 +0,0 @@
-import { AuthenticationError } from '@/domain/errors'
-import { AccessToken } from '@/domain/models'
-
-export type FacebookAuthenticationParams = {
-  token: string
-}
-
-export type FacebookAuthenticationResult = AccessToken | AuthenticationError

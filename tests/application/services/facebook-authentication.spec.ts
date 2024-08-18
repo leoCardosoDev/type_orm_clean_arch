ffc33bd866bd036e53ef2bbfd90bda800eb7d7ba
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-08-18 15:00:45 -0700

    test: ensure FacebookAuthenticationService rethow if LoadUserAccountRepository throws

--- tests/application/services/facebook-authentication.spec.ts
+++ tests/application/services/facebook-authentication.spec.ts
@@ -81,4 +81,10 @@ describe('Facebook Authentication Service', () => {
     const promise = sut.perform({ token })
     await expect(promise).rejects.toThrow(new Error('fb_error'))
   })
+
+  it('should rethrow if LoadUserAccountRepository throws', async () => {
+    userAccountRepository.load.mockRejectedValueOnce(new Error('fb_error'))
+    const promise = sut.perform({ token })
+    await expect(promise).rejects.toThrow(new Error('fb_error'))
+  })
 })

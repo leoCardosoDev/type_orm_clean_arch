2512b2c663200249c73973f5bef8cb4031924844
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-09-18 17:26:56 -0700

    test: ensure FacebookApi get debug token

--- tests/infra/apis/facebook-api.spec.ts
+++ tests/infra/apis/facebook-api.spec.ts
@@ -9,12 +9,13 @@ describe('FacebookApi', () => {
   let sut: FacebookApi
 
   beforeAll(() => {
-    httpClient = mock()
     clientId = 'any_client_id'
     clientSecret = 'any_client_secret'
+    httpClient = mock()
   })
 
   beforeEach(() => {
+    httpClient.get.mockResolvedValueOnce({ access_token: 'any_app_token' })
     sut = new FacebookApi(httpClient, clientId, clientSecret)
   })
 
@@ -26,6 +27,17 @@ describe('FacebookApi', () => {
         client_id: clientId,
         client_secret: clientSecret,
         grant_type: 'client_credentials'
+      }
+    })
+  })
+
+  it('should get debug token', async () => {
+    await sut.loadUser({ token: 'any_client_token' })
+    expect(httpClient.get).toHaveBeenCalledWith({
+      url: 'https://graph.facebook.com/debug_token',
+      params: {
+        access_token: 'any_app_token',
+        input_token: 'any_client_token'
       }
     })
   })

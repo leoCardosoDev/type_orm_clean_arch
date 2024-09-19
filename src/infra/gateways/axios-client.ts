c42ae01e6d1a3afc296885f367937d6b4eefacef
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-09-19 11:34:11 -0700

    feat: ensure AxiosHttpClient returns data on success

--- src/infra/gateways/axios-client.ts
+++ src/infra/gateways/axios-client.ts
@@ -3,7 +3,8 @@ import { HttpGetClientInput } from '@/infra/gateways'
 import axios from 'axios'
 
 export class AxiosHttpCLient {
-  async get(args: HttpGetClientInput): Promise<void> {
-    await axios.get(args.url, { params: args.params })
+  async get<T = any>(args: HttpGetClientInput): Promise<T> {
+    const result = await axios.get(args.url, { params: args.params })
+    return result.data
   }
 }

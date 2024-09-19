ce8f8d4fe735321009ce65af6e57561e6e3b3943
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-09-18 17:26:34 -0700

    feat: ensure FacebookApi get debug token

--- src/infra/apis/facebook-api.ts
+++ src/infra/apis/facebook-api.ts
@@ -10,13 +10,21 @@ export class FacebookApi {
     private readonly _clientSecret: string
   ) {}
 
-  async loadUser(_params: LoadFacebookUserApiParams): Promise<void> {
-    await this._httpClient.get({
+  async loadUser(params: LoadFacebookUserApiParams): Promise<void> {
+    const appToken = await this._httpClient.get({
       url: `${this.baseUrl}/oauth/access_token`,
       params: {
         client_id: this._clientId,
         client_secret: this._clientSecret,
         grant_type: 'client_credentials'
+      }
+    })
+
+    await this._httpClient.get({
+      url: `${this.baseUrl}/debug_token`,
+      params: {
+        access_token: appToken.access_token,
+        input_token: params.token
       }
     })
   }

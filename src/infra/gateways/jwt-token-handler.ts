459dfe30759cf90d19f13c7a96a497796c0fdebc
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-09-20 10:29:34 -0700

    feat: implements tokengenerator

--- src/infra/gateways/jwt-token-handler.ts
+++ src/infra/gateways/jwt-token-handler.ts
@@ -1,8 +1,8 @@
-import { TokenGeneratorParams, TokenGeneratorResult } from '@/application/contracts/cryptography'
+import { TokenGenerator, TokenGeneratorParams, TokenGeneratorResult } from '@/application/contracts/cryptography'
 
 import jwt from 'jsonwebtoken'
 
-export class JwtTokenHandler {
+export class JwtTokenHandler implements TokenGenerator {
   constructor(private readonly _secret: string) {}
   async generateToken(params: TokenGeneratorParams): Promise<TokenGeneratorResult> {
     const expirationInSeconds = params.expirationInMs / 1000

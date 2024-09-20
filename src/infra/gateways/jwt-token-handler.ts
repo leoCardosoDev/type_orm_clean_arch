b8a8d400fa677e01847b87e6c561a177ab865eaf
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-09-20 10:21:43 -0700

    feat: ensure JwtTokenHandler return a token on success

--- src/infra/gateways/jwt-token-handler.ts
+++ src/infra/gateways/jwt-token-handler.ts
@@ -1,11 +1,11 @@
-import { TokenGeneratorParams } from '@/application/contracts/cryptography'
+import { TokenGeneratorParams, TokenGeneratorResult } from '@/application/contracts/cryptography'
 
 import jwt from 'jsonwebtoken'
 
 export class JwtTokenHandler {
   constructor(private readonly _secret: string) {}
-  async generateToken(params: TokenGeneratorParams): Promise<void> {
+  async generateToken(params: TokenGeneratorParams): Promise<TokenGeneratorResult> {
     const expirationInSeconds = params.expirationInMs / 1000
-    jwt.sign({ key: params.key }, this._secret, { expiresIn: expirationInSeconds })
+    return jwt.sign({ key: params.key }, this._secret, { expiresIn: expirationInSeconds })
   }
 }

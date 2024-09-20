afe7b692b78feabbadb44d5ab30eea5e76680dbf
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-09-20 10:17:14 -0700

    refactor: isolate SUT creation

--- tests/infra/gateways/jwt-token-handler.spec.ts
+++ tests/infra/gateways/jwt-token-handler.spec.ts
@@ -5,9 +5,18 @@ import jwt from 'jsonwebtoken'
 jest.mock('jsonwebtoken')
 
 describe('JwtTokenHandler', () => {
+  let sut: JwtTokenHandler
+  let fakeJwt: jest.Mocked<typeof jwt>
+
+  beforeAll(() => {
+    fakeJwt = jwt as jest.Mocked<typeof jwt>
+  })
+
+  beforeEach(() => {
+    sut = new JwtTokenHandler('any_secret')
+  })
+
   it('should call sign with correct params', async () => {
-    const fakeJwt = jwt as jest.Mocked<typeof jwt>
-    const sut = new JwtTokenHandler('any_secret')
     await sut.generateToken({ key: 'any_key', expirationInMs: 1000 })
     expect(fakeJwt.sign).toHaveBeenCalledWith({ key: 'any_key' }, 'any_secret', { expiresIn: 1 })
   })

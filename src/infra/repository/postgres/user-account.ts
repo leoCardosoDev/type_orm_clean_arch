b3533d021d0732eef68cff642fbf5255c74bb2f8
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-09-20 17:20:16 -0700

    feat: ensure PgUserAccountRepositor loads account if email exists

---
+++ src/infra/repository/postgres/user-account.ts
@@ -0,0 +1,17 @@
+import { LoadUserAccountRepository, LoadUserAccountRepositoryParams, LoadUserAccountRepositoryResult } from '@/application/contracts/repositories'
+import { DataSource } from 'typeorm'
+import { PgUser } from '@/infra/repository/postgres/entities'
+
+export class PgUserAccountRepository implements LoadUserAccountRepository {
+  constructor(private readonly _dataSource: DataSource) {}
+  async load(param: LoadUserAccountRepositoryParams): Promise<LoadUserAccountRepositoryResult> {
+    const pgUserRepository = this._dataSource.getRepository(PgUser)
+    const pgUser = await pgUserRepository.findOne({ where: { email: param.email } })
+    if (pgUser !== undefined) {
+      return {
+        id: pgUser!.id.toString(),
+        name: pgUser?.name ?? undefined
+      }
+    }
+  }
+}

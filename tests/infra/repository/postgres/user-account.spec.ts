719af0a8ce2b1a2dd92ff91f99689f1e572dd7b0
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-09-20 19:28:02 -0700

    test: ensure PgUserAccountRepository return account id

--- tests/infra/repository/postgres/user-account.spec.ts
+++ tests/infra/repository/postgres/user-account.spec.ts
@@ -43,13 +43,14 @@ describe('PgUserAccountRepository', () => {
 
   describe('saveWithFacebook', () => {
     it('should create an account if id is undefined', async () => {
-      await sut.saveWithFacebook({
+      const { id } = await sut.saveWithFacebook({
         email: 'any_email',
         name: 'any_name',
         facebookId: 'any_fb_id'
       })
       const pgUser = await pgUserRepository.findOne({ where: { email: 'any_email' } })
       expect(pgUser?.id).toBe(1)
+      expect(id).toBe('1')
     })
 
     it('should update an account if id is defined', async () => {
@@ -58,7 +59,7 @@ describe('PgUserAccountRepository', () => {
         name: 'any_name',
         facebookId: 'any_fb_id'
       })
-      await sut.saveWithFacebook({
+      const { id } = await sut.saveWithFacebook({
         id: '1',
         email: 'new_email',
         name: 'new_name',
@@ -66,6 +67,7 @@ describe('PgUserAccountRepository', () => {
       })
       const pgUser = await pgUserRepository.findOne({ where: { id: 1 } })
       expect(pgUser).toEqual({ id: 1, email: 'any_email', name: 'new_name', facebookId: 'new_fb_id' })
+      expect(id).toBe('1')
     })
   })
 })

dc10a4e6670129d8d1356344fc8aacffe043aefb
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-09-20 17:29:03 -0700

    test: ensure PgUserAccountRepository returns undefined if email not exists

--- tests/infra/repository/postgres/user-account.spec.ts
+++ tests/infra/repository/postgres/user-account.spec.ts
@@ -33,6 +33,37 @@ describe('PgUserAccountRepository', () => {
       const sut = new PgUserAccountRepository(connection)
       const account = await sut.load({ email: 'existing_email' })
       expect(account).toEqual({ id: '1' })
+      await connection.close()
+    })
+
+    it('should return undefine if email does not exists', async () => {
+      const db = newDb()
+      db.public.registerFunction({
+        name: 'current_database',
+        returns: db.public.getType(DataType.text),
+        implementation: () => 'pg_mem_test_db' // Retorna um nome fictício de banco de dados
+      })
+      db.public.registerFunction({
+        name: 'version',
+        returns: db.public.getType(DataType.text), // Retorna o tipo text
+        implementation: () => 'PostgreSQL 13.3' // Retorna uma string fictícia com a versão
+      })
+      db.public.registerFunction({
+        name: 'obj_description',
+        args: [DataType.regclass, DataType.text], // Tipos esperados para os argumentos
+        returns: db.public.getType(DataType.text),
+        implementation: () => null // Retorna null ou uma string fictícia
+      })
+      const connection = await db.adapters.createTypeormDataSource({
+        type: 'postgres',
+        entities: [PgUser]
+      })
+      await connection.initialize()
+      await connection.synchronize()
+      const sut = new PgUserAccountRepository(connection)
+      const account = await sut.load({ email: 'new_email' })
+      expect(account).toBeUndefined()
+      await connection.close()
     })
   })
 })

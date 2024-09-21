2c41d3e6abfffd6006db5715bb8489bb3ea6257c
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-09-20 19:03:44 -0700

    feat: ensure PgUserAccountRepository creates an account if id is undefined

--- src/infra/repository/postgres/user-account.ts
+++ src/infra/repository/postgres/user-account.ts
@@ -1,9 +1,10 @@
-import { LoadUserAccountRepository, LoadUserAccountRepositoryParams, LoadUserAccountRepositoryResult } from '@/application/contracts/repositories'
+import { LoadUserAccountRepository, LoadUserAccountRepositoryParams, LoadUserAccountRepositoryResult, SaveFacebookAccountRepositoryParams } from '@/application/contracts/repositories'
 import { DataSource } from 'typeorm'
 import { PgUser } from '@/infra/repository/postgres/entities'
 
 export class PgUserAccountRepository implements LoadUserAccountRepository {
   constructor(private readonly _dataSource: DataSource) {}
+
   async load(param: LoadUserAccountRepositoryParams): Promise<LoadUserAccountRepositoryResult> {
     const pgUserRepository = this._dataSource.getRepository(PgUser)
     const pgUser = await pgUserRepository.findOne({ where: { email: param.email } })
@@ -12,5 +13,14 @@ export class PgUserAccountRepository implements LoadUserAccountRepository {
       id: pgUser!.id.toString(),
       name: pgUser?.name ?? undefined
     }
+  }
+
+  async saveWithFacebook(params: SaveFacebookAccountRepositoryParams): Promise<void> {
+    const pgUserRepository = this._dataSource.getRepository(PgUser)
+    await pgUserRepository.save({
+      email: params.email,
+      name: params.name,
+      facebookId: params.facebookId
+    })
   }
 }

4dcee45bbc0b4fb317ab57abc1450cf03c178715
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-09-20 18:44:16 -0700

    refactor: move inMemoryDb Creation to its own file

--- tests/infra/repository/postgres/user-account.spec.ts
+++ tests/infra/repository/postgres/user-account.spec.ts
@@ -1,40 +1,19 @@
 import { PgUserAccountRepository } from '@/infra/repository/postgres'
 import { PgUser } from '@/infra/repository/postgres/entities'
-import { DataType, IBackup, IMemoryDb, newDb } from 'pg-mem'
+import { IBackup } from 'pg-mem'
 import { DataSource, Repository } from 'typeorm'
+import { makeFakeConnection, makeFakeDb } from '@/tests/infra/repository/postgres/mocks'
 
 describe('PgUserAccountRepository', () => {
   describe('load', () => {
     let sut: PgUserAccountRepository
     let pgUserRepository: Repository<PgUser>
     let connection: DataSource
-    let db: IMemoryDb
     let backup: IBackup
 
     beforeAll(async () => {
-      db = newDb()
-      db.public.registerFunction({
-        name: 'current_database',
-        returns: db.public.getType(DataType.text),
-        implementation: () => 'pg_mem_test_db'
-      })
-      db.public.registerFunction({
-        name: 'version',
-        returns: db.public.getType(DataType.text),
-        implementation: () => 'PostgreSQL 13.3'
-      })
-      db.public.registerFunction({
-        name: 'obj_description',
-        args: [DataType.regclass, DataType.text],
-        returns: db.public.getType(DataType.text),
-        implementation: () => null
-      })
-      connection = await db.adapters.createTypeormDataSource({
-        type: 'postgres',
-        entities: [PgUser]
-      })
-      await connection.initialize()
-      await connection.synchronize()
+      const db = await makeFakeDb()
+      connection = await makeFakeConnection(db, [PgUser])
       backup = db.backup()
       pgUserRepository = connection.getRepository(PgUser)
     })
@@ -49,6 +28,7 @@ describe('PgUserAccountRepository', () => {
       backup.restore()
       sut = new PgUserAccountRepository(connection)
     })
+
     it('should return an account if email exists', async () => {
       await pgUserRepository.save({ email: 'existing_email' })
       const account = await sut.load({ email: 'existing_email' })

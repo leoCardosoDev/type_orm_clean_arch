a9056d4aad1551f3b7947430ec56bbe796bd69d0
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-09-24 09:13:44 -0700

    feat: ensure FacebookLoginController returns 200 if authentication fails

--- src/application/controllers/facebook-login.ts
+++ src/application/controllers/facebook-login.ts
@@ -1,6 +1,7 @@
 import { HttpResponse } from '@/application/helpers'
 import { AuthenticationError } from '@/domain/errors'
 import { FacebookAuthentication } from '@/domain/features'
+import { AccessToken } from '@/domain/models'
 
 export class FacebookLoginController {
   constructor(private readonly _facebookAuthentication: FacebookAuthentication) {}
@@ -12,10 +13,17 @@ export class FacebookLoginController {
         data: new Error('The field token is required')
       }
     }
-    await this._facebookAuthentication.perform({ token: httpRequest.token })
-    return {
-      statusCode: 401,
-      data: new AuthenticationError()
+    const result = await this._facebookAuthentication.perform({ token: httpRequest.token })
+    if (result instanceof AccessToken) {
+      return {
+        statusCode: 200,
+        data: { accessToken: result._value }
+      }
+    } else {
+      return {
+        statusCode: 401,
+        data: new AuthenticationError()
+      }
     }
   }
 }

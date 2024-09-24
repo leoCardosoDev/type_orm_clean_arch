f9a4ffe3ef6a8fea8a5a2c8937c2a4ad4f620141
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-09-24 09:04:59 -0700

    feat: ensure FacebookLoginController returns 401 if authentication fails

--- src/application/controllers/facebook-login.ts
+++ src/application/controllers/facebook-login.ts
@@ -1,14 +1,21 @@
 import { HttpResponse } from '@/application/helpers'
+import { AuthenticationError } from '@/domain/errors'
 import { FacebookAuthentication } from '@/domain/features'
 
 export class FacebookLoginController {
   constructor(private readonly _facebookAuthentication: FacebookAuthentication) {}
 
   async handle(httpRequest: any): Promise<HttpResponse> {
+    if (httpRequest.token === '' || httpRequest.token === null || httpRequest.token === undefined) {
+      return {
+        statusCode: 400,
+        data: new Error('The field token is required')
+      }
+    }
     await this._facebookAuthentication.perform({ token: httpRequest.token })
     return {
-      statusCode: 400,
-      data: new Error('The field token is required')
+      statusCode: 401,
+      data: new AuthenticationError()
     }
   }
 }

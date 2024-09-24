5b5d1e241ff9b28ef31ce15b34e9b5898fca66d6
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-09-24 08:49:11 -0700

    feat: ensure FacebookLoginController calls FacebookAuthentication with correct params

--- src/application/controllers/facebook-login.ts
+++ src/application/controllers/facebook-login.ts
@@ -1,7 +1,11 @@
 import { HttpResponse } from '@/application/helpers'
+import { FacebookAuthentication } from '@/domain/features'
 
 export class FacebookLoginController {
-  async handle(_httpRequest: any): Promise<HttpResponse> {
+  constructor(private readonly _facebookAuthentication: FacebookAuthentication) {}
+
+  async handle(httpRequest: any): Promise<HttpResponse> {
+    await this._facebookAuthentication.perform({ token: httpRequest.token })
     return {
       statusCode: 400,
       data: new Error('The field token is required')

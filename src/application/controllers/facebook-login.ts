398368e7aa9532fb9665817dff7670fba2a11d8a
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-09-25 11:53:15 -0700

    feat: ensure FacebookLoginController returns 500 if authentication throws

--- src/application/controllers/facebook-login.ts
+++ src/application/controllers/facebook-login.ts
@@ -2,28 +2,33 @@ import { HttpResponse } from '@/application/helpers'
 import { AuthenticationError } from '@/domain/errors'
 import { FacebookAuthentication } from '@/domain/features'
 import { AccessToken } from '@/domain/models'
+import { ServerError } from '../errors'
 
 export class FacebookLoginController {
   constructor(private readonly _facebookAuthentication: FacebookAuthentication) {}
 
   async handle(httpRequest: any): Promise<HttpResponse> {
-    if (httpRequest.token === '' || httpRequest.token === null || httpRequest.token === undefined) {
-      return {
-        statusCode: 400,
-        data: new Error('The field token is required')
+    try {
+      if (httpRequest.token === '' || httpRequest.token === null || httpRequest.token === undefined) {
+        return {
+          statusCode: 400,
+          data: new Error('The field token is required')
+        }
       }
-    }
-    const result = await this._facebookAuthentication.perform({ token: httpRequest.token })
-    if (result instanceof AccessToken) {
-      return {
-        statusCode: 200,
-        data: { accessToken: result._value }
+      const result = await this._facebookAuthentication.perform({ token: httpRequest.token })
+      if (result instanceof AccessToken) {
+        return {
+          statusCode: 200,
+          data: { accessToken: result._value }
+        }
+      } else {
+        return {
+          statusCode: 401,
+          data: new AuthenticationError()
+        }
       }
-    } else {
-      return {
-        statusCode: 401,
-        data: new AuthenticationError()
-      }
+    } catch (error) {
+      return { statusCode: 500, data: new ServerError(error as Error) }
     }
   }
 }

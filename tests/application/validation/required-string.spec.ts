bc128bd5ef1370aa5325d0078584377c57066cf8
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-09-25 13:00:28 -0700

    test: ensure RequiredStringValidation returns undefinedf in correct value

--- tests/application/validation/required-string.spec.ts
+++ tests/application/validation/required-string.spec.ts
@@ -14,9 +14,9 @@ describe('RequiredStringValidation', () => {
     expect(error).toEqual(new RequiredFieldError('any_field'))
   })
 
-  it('should return RequiredFieldError if value is undefined', () => {
-    const sut = new RequiredStringValidation(undefined as any, 'any_field')
+  it('should return undefined if value is not empty', () => {
+    const sut = new RequiredStringValidation('any_value', 'any_field')
     const error = sut.validate()
-    expect(error).toEqual(new RequiredFieldError('any_field'))
+    expect(error).toBeUndefined()
   })
 })

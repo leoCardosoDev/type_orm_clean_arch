d4999d8e3e246cc3158afb0b0a7aa4f2e52872ca
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-09-25 13:00:07 -0700

    feat: ensure RequiredStringValidation returns undefinedf in correct value

--- src/application/validation/required-string.ts
+++ src/application/validation/required-string.ts
@@ -1,11 +1,14 @@
-import { RequiredFieldError } from '../errors'
+import { RequiredFieldError } from '@/application/errors'
 
 export class RequiredStringValidation {
   constructor(
     private readonly _value: string,
     private readonly _fieldName: string
   ) {}
+
   validate(): Error | undefined {
-    return new RequiredFieldError('any_field')
+    if (this._value === '' || this._value === null || this._value === undefined) {
+      return new RequiredFieldError(this._fieldName)
+    }
   }
 }

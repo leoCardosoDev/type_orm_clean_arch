a9223bbb1cbba8504e4e876184a98fad3176af98
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-09-26 07:06:01 -0700

    refactor: isolate SUT creation

--- tests/application/validation/composite.spec.ts
+++ tests/application/validation/composite.spec.ts
@@ -1,14 +1,25 @@
 import { ValidationComposite, Validator } from '@/application/validation'
-import { mock } from 'jest-mock-extended'
+import { mock, MockProxy } from 'jest-mock-extended'
 
 describe('ValidateComposite', () => {
-  it('should return undefined if all Validatiors return undefined', () => {
-    const validator1 = mock<Validator>()
+  let sut: ValidationComposite
+  let validator1: MockProxy<Validator>
+  let validator2: MockProxy<Validator>
+  let validators: Validator[]
+
+  beforeAll(() => {
+    validator1 = mock()
+    validator2 = mock()
     validator1.validate.mockReturnValue(undefined)
-    const validator2 = mock<Validator>()
     validator2.validate.mockReturnValue(undefined)
-    const validators = [validator1, validator2]
-    const sut = new ValidationComposite(validators)
+    validators = [validator1, validator2]
+  })
+
+  beforeEach(() => {
+    sut = new ValidationComposite(validators)
+  })
+
+  it('should return undefined if all Validatiors return undefined', () => {
     const error = sut.validate()
     expect(error).toBeUndefined()
   })

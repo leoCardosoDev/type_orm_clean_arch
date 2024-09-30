3dc2b2720e2ee833bcce83bf2de2847aa2f8694a
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-09-30 09:07:09 -0700

    test: ensure FacebookApi returns undefined when HttpGetClient throws

--- tests/external/facebook-api.test.ts
+++ tests/external/facebook-api.test.ts
@@ -1,5 +1,23 @@
+import { FacebookApi } from '@/infra/apis'
+import { AxiosHttpCLient } from '@/infra/gateways'
+import { env } from '@/main/config/env'
+
 describe('Facebook Api integration Tests', () => {
-  it('Não pode rodar nos testes gerais', () => {
-    expect(1).toBe(1)
+  it('should return a Facebook User if token is valid', async () => {
+    const axiosClient = new AxiosHttpCLient()
+    const sut = new FacebookApi(axiosClient, env.facebookApi.clientId, env.facebookApi.clientSecret)
+    const fbUser = await sut.loadUser({ token: env.facebookApi.accessToken })
+    expect(fbUser).toEqual({
+      facebookId: '8544711292255677',
+      name: 'Leonardo De Oliveira Silva',
+      email: 'leowkfmc@hotmail.com'
+    })
+  })
+
+  it('should return a undefined if token is invalid', async () => {
+    const axiosClient = new AxiosHttpCLient()
+    const sut = new FacebookApi(axiosClient, env.facebookApi.clientId, env.facebookApi.clientSecret)
+    const fbUser = await sut.loadUser({ token: 'invalid' })
+    expect(fbUser).toBeUndefined()
   })
 })

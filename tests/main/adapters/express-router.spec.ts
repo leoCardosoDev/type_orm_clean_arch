5632072ac1286989a6a6816d0d76c849decb18fd
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-10-01 07:14:14 -0700

    test: ensure ExpressRouter calls handler with correct request

---
+++ tests/main/adapters/express-router.spec.ts
@@ -0,0 +1,16 @@
+import { Controller } from '@/application/controllers'
+import { ExpressRouter } from '@/main/adapters'
+import { getMockReq, getMockRes } from '@jest-mock/express'
+import { mock, MockProxy } from 'jest-mock-extended'
+
+describe('ExpressRouter', () => {
+  it('should call handle with correct request', async () => {
+    const req = getMockReq({ body: { any: 'any' } })
+    const { res } = getMockRes()
+    let controller: MockProxy<Controller>
+    controller = mock()
+    const sut = new ExpressRouter(controller)
+    await sut.adapt(req, res)
+    expect(controller.handle).toHaveBeenCalledWith({ any: 'any' })
+  })
+})

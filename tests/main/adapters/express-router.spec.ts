6340cecf05c7fc87ccf2e103572a17e39311ae83
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-10-01 07:15:56 -0700

    test: ensure ExpressRouter calls handler with empty request

--- tests/main/adapters/express-router.spec.ts
+++ tests/main/adapters/express-router.spec.ts
@@ -13,4 +13,14 @@ describe('ExpressRouter', () => {
     await sut.adapt(req, res)
     expect(controller.handle).toHaveBeenCalledWith({ any: 'any' })
   })
+
+  it('should call handle with empty request', async () => {
+    const req = getMockReq()
+    const { res } = getMockRes()
+    let controller: MockProxy<Controller>
+    controller = mock()
+    const sut = new ExpressRouter(controller)
+    await sut.adapt(req, res)
+    expect(controller.handle).toHaveBeenCalledWith({})
+  })
 })

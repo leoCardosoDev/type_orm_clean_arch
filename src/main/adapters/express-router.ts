8ecc40faa3300520b28512cafbc6b5e98e38395e
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-10-01 07:55:27 -0700

    feat: ensure ExpressRouter responds 400 with error message

--- src/main/adapters/express-router.ts
+++ src/main/adapters/express-router.ts
@@ -5,6 +5,10 @@ export class ExpressRouter {
   constructor(private readonly _controller: Controller) {}
   async adapt(req: Request, res: Response): Promise<void> {
     const httpResponse = await this._controller.handle({ ...req.body })
-    res.status(200).json(httpResponse.data)
+    if (httpResponse.statusCode === 200) {
+      res.status(200).json(httpResponse.data)
+    } else {
+      res.status(400).json({ error: httpResponse.data.message })
+    }
   }
 }

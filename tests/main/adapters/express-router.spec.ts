35a748bef26a95de3e603f27e1815325888e4822
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-10-01 07:59:20 -0700

    test: ensure ExpressRouter responds 500 with error message

--- tests/main/adapters/express-router.spec.ts
+++ tests/main/adapters/express-router.spec.ts
@@ -61,4 +61,16 @@ describe('ExpressRouter', () => {
     expect(res.json).toHaveBeenCalledWith({ error: 'any_error' })
     expect(res.json).toHaveBeenCalledTimes(1)
   })
+
+  it('should respond with 500 and error message', async () => {
+    controller.handle.mockResolvedValueOnce({
+      statusCode: 500,
+      data: new Error('any_error')
+    })
+    await sut.adapt(req, res)
+    expect(res.status).toHaveBeenCalledWith(500)
+    expect(res.status).toHaveBeenCalledTimes(1)
+    expect(res.json).toHaveBeenCalledWith({ error: 'any_error' })
+    expect(res.json).toHaveBeenCalledTimes(1)
+  })
 })
